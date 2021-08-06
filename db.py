203778140bc8c9f4f09fcf8ddc5e5e906214f6d6
shivendra0802 <83053524+shivendra0802@users.noreply.github.com>
2021-08-06 19:59:43 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+
+print("opened database successfully")
+
+conn.execute("CREATE TABLE students (name TEXT,addr TEXT,city TEXT, pin TEXT)")
+
+print("table created successfully")
